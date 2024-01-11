c78f38f5fe8ae1c99be4a1dfe063aab7bb9a0e97
boytrend0108 <boytrend@gmail.com>
2024-01-11 20:49:19 +0200

    implement reduser 2.0

--- ReactTs/react_context/src/components/Main/Main.tsx
+++ ReactTs/react_context/src/components/Main/Main.tsx
@@ -1,16 +1,63 @@
-import { useState } from "react";
+import { useReducer} from "react";
 import { HomePage } from "../HomePage/HomePage";
+import { Lang } from "../../types/Lang";
 
+type Action  = {type: 'increase'}
+             | {type: 'decrease'}
+             | {type: 'add',  payload: number,}
+             | {type: 'lang'}
+ 
+
+interface State {
+  value: number,
+  lang: Lang,
+}
+
+function reducer(state: State, action: Action): State {
+   switch (action.type) {
+     case 'increase':
+       return {
+         ...state, 
+         value: state.value + 1,
+       }
+     case 'decrease':
+       return {
+         ...state, 
+         value: state.value - 1,
+       };
+     case 'add':
+       return {
+         ...state,
+         value: state.value + action.payload,
+       }
+      case 'lang':
+        return {
+          ...state,
+          lang: state.lang,
+        }
+     default: 
+       return state;
+   }
+}
+
+const initialState: State = {
+  value: 0,
+  lang: Lang.EN
+}
 
 export const Main: React.FC= () => {
-  const [counter, setCounter] = useState(0);
+  const [{value: counter}, dispatch] = useReducer(reducer, initialState);
    
   const increase = () => {
-    setCounter(current => current + 1);
+    dispatch({type: 'increase'});
   }
 
   const decrease = () => {
-    setCounter(current => current - 1);
+    dispatch({type: 'decrease'});
+  };
+
+  const add = () => {
+    dispatch({type: 'add', payload: 10})
   }
   return (
     <main>
@@ -19,6 +66,7 @@ export const Main: React.FC= () => {
       <button onClick={decrease}>-</button>
         {counter}
       <button onClick={increase}>+</button>
+      <button onClick={add}>+10</button>
     </main>
   );
 }
